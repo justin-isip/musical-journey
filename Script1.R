48e839572db506ddb77bfd13fb70a20773858c5e
Justin Isip <justinisip@outlook.com>
2023-01-18 12:09:55 +0000

    Added code

--- Script1.R
+++ Script1.R
@@ -1,3 +1,20 @@
 # Script trial
 
 # Hello, welcome to my script!
+
+for (i in 1:10) {
+  if(i > 1) 
+  print(i)
+}
+
+a <- 5
+b <- 10
+
+ifelse(a < b, print(a), print(b))
+
+if (a < b) {
+  print(a)
+} else {
+  print(b)
+}
+
